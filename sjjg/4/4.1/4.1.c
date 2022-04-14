1fe9e10a9dcff73c2ec7937a42d2393e9979a701
cake <cake.peng@photonpay.com>
2022-04-14 20:44:50 +0800

    串算法匹配

---
+++ sjjg/4/4.1/4.1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 255 // 预定串的长度是 255
+
+// 静态数组实现（定长顺序存储）
+typedef struct {
+    char ch[MAXSIZE]; // 每个分量存储一个字符，系统会自动回收空间
+    int length; // 串的实际长度
+}SString;
+
+typedef struct StringNode {
+    char ch; // 每个节点存1个字符,1b
+    struct StringNode * next; // 4个字节存储位置信息
+}StringNode, * String; // 【存储密度低】每个字符1b，每个指针4b；
+
+// 解决存储密度低的问题，可以让每个结点存多个字符。存储密度更高
+typedef struct StringNode2 {
+    char ch[4]; // 每个节点存4个字符,1b
+    struct StringNode2 * next; // 4个字节存储位置信息
+}StringNode2, * String2;
+
+// 动态数组实现（堆分配存储）
+typedef struct {
+    char *ch;
+    int length; // 串的实际长度
+}HString;
+
+int main() {
+    HString S;
+    S.ch = (char *)malloc(MAXSIZE * sizeof(char)); // 从堆里分配内存，用完需要手动free
+
+    char c[MAXSIZE];
+    printf("sizeof c %lu \n", sizeof(char));
+    printf("sizeof c %lu", sizeof(c)/sizeof(char));
+}
---
+++ sjjg/4/4.1/4.1_2.c
@@ -0,0 +1,135 @@
+/**
+ * @file 4.1_2.c 基本操作实现，用方案4
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAXSIZE 255 // 预定串的长度是 255
+
+// 静态数组实现（定长顺序存储）
+typedef struct {
+    char ch[MAXSIZE]; // 每个分量存储一个字符，系统会自动回收空间
+    int length; // 串的实际长度
+}SString;
+
+/**
+ * @brief 求串长。返回串S的元素个数。
+ * 
+ * @return int 
+ */
+int StrLength(SString);
+
+/**
+ * @brief 清空操作。将S清空为空串。—— 空间还能用
+ * 
+ */
+void ClearString(SString *);
+
+/**
+ * @brief Construct a new Sub String object
+ * 求子串。用Sub返回串S的第pos个字符起长度为len的子串
+ * 
+ */
+_Bool SubString(SString *, SString, int, int);
+
+/**
+ * @brief 清空操作。将S清空为空串。—— 空间还能用
+ * 
+ * @return _Bool 
+ */
+_Bool StrEmpty(SString);
+
+/**
+ * @brief 比较操作。若 S>T，则返回值 > 0；若S=T，则返回值=0；如果S < T , 则返回值 < 0。
+ * 
+ */
+int StrCompare(SString, SString);
+
+/**
+ * @brief 定位操作。若主串S中存在与串T值相同的子串，则返回它在主串S中第一次出现的位置；否则函数为0.
+ * 
+ * @return int 
+ */
+int Index(SString, SString);
+
+
+/**
+ * @brief 赋值操作。把串T赋值为chars
+ * 
+ */
+_Bool StrAssign(SString *, char[]);
+
+int main() {
+    SString S;
+    SString Sub;
+    S.length = 4;
+    S.ch[1] = 'c';
+    S.ch[2] = 'b';
+    S.ch[3] = 'd';
+    S.ch[4] = 'f';
+
+    if (SubString(&Sub, S, 3, 1)) {
+        printf("成功地成功了 %c \n", S.ch[2]);
+        printf("成功地成功了 %c \n", Sub.ch[1]);
+    }
+    printf("比较是否相等 %d \n", StrCompare(S, Sub));
+    printf("获取对应下标 %d \n", Index(S, Sub));
+}
+
+int StrLength(SString S)
+{
+    return S.length;
+}
+
+void ClearString(SString * S)
+{
+    S->length = 0;
+}
+
+_Bool StrEmpty(SString S)
+{
+    if (S.length == 0)
+        return true;
+    else
+        return false;
+}
+
+_Bool SubString(SString * Sub, SString S, int pos, int len)
+{
+    // 子串范围越界
+    if (pos + len - 1 > S.length) {
+        return false;
+    }
+    for (int i = pos;i<= pos + len -1; i++) {
+        Sub->ch[i-pos+1] = S.ch[i];
+    }
+    Sub->length = len;
+    return true;
+}
+
+int StrCompare(SString S, SString T) {
+    for (int i = 1; i <= S.length && i <= T.length; i++) {
+        if (S.ch[i] != T.ch[i])
+            return S.ch[i] - T.ch[i];
+    }
+    return S.length - T.length;
+}
+
+int Index(SString S, SString T) {
+    int i=1, n =StrLength(S), m=StrLength(T);
+    SString Sub;
+    while ( i<=n-m+1)
+    {
+        SubString(&Sub, S, i, m);
+        if (StrCompare(Sub, T) != 0)  ++i;
+        else return i; // 返回子串在主串中的位置
+    }
+    return 0;
+}
