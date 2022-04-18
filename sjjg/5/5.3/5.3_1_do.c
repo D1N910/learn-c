85d2989dffc134cc41047ce7838f27d9589b0467
cake <cake.peng@photonpay.com>
2022-04-18 19:47:26 +0800

    feat: fix

--- sjjg/5/5.3/5.3_1_do.c
+++ sjjg/5/5.3/5.3_1_do.c
@@ -6,60 +6,53 @@
 typedef struct BinaryTree
 {
     int data; // 存储的值
-    struct BinaryTree * left, * right;
+    struct BinaryTree * left;
+    struct BinaryTree * right;
 }BinaryTree, *LBinaryTree;
 
 // 初始化树
-void IntTree(LBinaryTree, int x);
+void IntTree(LBinaryTree *);
 
 // 添加值
 void AddValue(LBinaryTree, int);
 
 int main(void){
-    BinaryTree T;
-    T.data = -1;
-    T.left = NULL;
-    T.right = NULL;
-    AddValue(&T, 20);
-    AddValue(&T, 21);
-    // AddValue(&T, 22);
-    printf("get data %d \n", T.data);
+    LBinaryTree T = NULL;
+    IntTree(&T);
+    AddValue(T, 20);
+    AddValue(T, 21);
+    AddValue(T, 19);
+    AddValue(T, 22);
+    printf("get data %d \n", T->data);
+    printf("get data %d \n", T->left->data);
+    printf("get data %d \n", T->right->data);
     return 0;
 }
 
-void IntTree(LBinaryTree T, int x)
+void IntTree(LBinaryTree *T)
 {
-    if (T != NULL) {
-        printf("IntTree (T != NULL) %d \n", x);
+    if (*T != NULL) {
         return;
     }
-    printf("IntTree (T == NULL) %d %d \n", T == NULL, x);
-    T = (LBinaryTree)malloc(sizeof(BinaryTree));
-    T->data = -1;
-    T->left = NULL;
-    T->right = NULL;
-    printf("IntTree (T == NULL) %d %d \n", T == NULL, x);
+    *T = (LBinaryTree)malloc(sizeof(BinaryTree));
+    (*T)->data = -1;
+    (*T)->left = NULL;
+    (*T)->right = NULL;
 }
 
 void AddValue(LBinaryTree T, int x)
 {
     if (T == NULL) {
-        printf("AddValue (T == NULL) %d \n", x);
         return;
     }
-    printf("AddValue (T != NULL) %d \n", x);
-    printf("AddValue Get Now Data %d \n", T->data);
     if (T->data == -1) {
         T->data = x;
     } else {
         if (x < T->data) {
-            printf("x < T.data %d \n", x);
-            IntTree(T->left, x);
+            IntTree(&(T->left));
             AddValue(T->left, x);
         } else {
-            printf("x < T.data %d \n", x);
-            IntTree(T->right, x);
-            printf("x < T.data (T == NULL) %d %d \n", T->right == NULL, x);
+            IntTree(&(T->right));
             AddValue(T->right, x);
         }
     }
